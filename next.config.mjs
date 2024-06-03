cb1f7a63725c0e21b44760be96b13c8f9ab0923c
Reza Kamali <mrgando@protonmail.com>
2024-06-03 12:40:36 +0330

    second try on deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
+/** @type {import('next').NextConfig} */
 import { withCss } from 'next-css';
 
 export default withCss({
-  webpack(config, { isServer }) {
-    // Add support for CSS
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
+    webpack(config, { isServer }) {
+        // Add support for CSS
+        config.module.rules.push({
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        });
 
-    return config;
-  },
+        return config;
+    },
 });
