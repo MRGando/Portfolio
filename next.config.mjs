a0c2243fccd7fadb936d3f6955f0dc9449a46581
Reza Kamali <mrgando@protonmail.com>
2024-06-03 12:44:16 +0330

    bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withCSS = require('@zeit/next-css')
-const withLess = require('@zeit/next-less')
-const withSass = require('@zeit/next-sass')
+const nextConfig = {};
 
-if (typeof require !== 'undefined') {
-  require.extensions['.less'] = () => {}
-}
-
-module.exports = withCSS(
-  withLess(
-    withSass({
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-      webpack: config => {
-        config.module.rules.push(
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          }
-        );
-        return config;
-      }
-    })
-  )
-)
+export default nextConfig;
